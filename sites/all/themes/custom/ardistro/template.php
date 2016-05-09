71a947414a472ca9cacd11cb98315acc4cf49417
Horacio Alvarado <halvarado@minem.gob.ar>
2016-05-09 13:54:57 -0300

    Cambio nombres bloques, agregado modulo coffee

--- sites/all/themes/custom/ardistro/template.php
+++ sites/all/themes/custom/ardistro/template.php
@@ -7,3 +7,15 @@
 function ardistro_preprocess(&$variables, $hook) {
 	// krumo($hook);
 }
+
+/**
+ * THEME_preprocess_image_style() is also available.
+ */
+function ardistro_preprocess_image(&$variables) {
+  if(isset($variables['style_name'])) {
+    if($variables['style_name'] == 'articulo_thumbnail') {
+      $variables['attributes']['class'][] = "img-thumbnail";
+    }
+  }
+  //var_dump($variables);
+}
